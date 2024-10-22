fac500bace2734de97e4d2e32678c020aaeab6b0
gustave <gustave.jacq@gmail.com>
2024-10-22 12:14:27 +0200

    test 1er push

--- src/test/java/fr/efrei/pokemon/PokemonApplicationTests.java
+++ src/test/java/fr/efrei/pokemon/PokemonApplicationTests.java
@@ -2,7 +2,7 @@ package fr.efrei.pokemon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//test
 @SpringBootTest
 class PokemonApplicationTests {
 
